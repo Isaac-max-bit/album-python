df8c2c8aaedaa96daa553fee3d4842de47c48e31
Isaac Garcia <garciaalejoisaac@gmail.com>
2025-02-21 22:46:32 -0500

    new content

---
+++ clase6.py
@@ -0,0 +1,44 @@
+# Operadores numéricos
+
+a = 10
+b = 3
+
+print("La suma es: ", a + b)
+print("La resta es: ", a - b)
+print("La multiplicación es: ", a * b)
+print("El módulo es: ", a % b)
+print("Potenciación: ", a ** b)
+print("La división es: ", a / b)
+print("parte entera de la división es: ", a // b)
+
+
+# a = a + 2
+# print(a)
+
+# a += 2
+# print(a) 
+
+# a -= 2
+# print(a)
+
+# a *= 2
+# print(a)
+
+# a /= 2
+# print(a)
+
+operation_1 = 2 + 3 * 4
+
+operation_2 = 2 + (3 * 4)
+
+operation_3 = (2 + 3) * 4
+
+operation_4 = (2+3) * (4**2)/ 8 - 1
+
+print(operation_1)
+
+print(operation_2)
+
+print(operation_3)
+
+print(operation_4)
